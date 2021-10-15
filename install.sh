f104fd298c61db5211b39c98f879946193807344
whiteness0 <92606867+whiteness0@users.noreply.github.com>
2021-10-16 04:52:15 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 checkroot() {
 
 if [[ "$(id -u)" -ne 0 ]]; then
-   printf "\e[1;77mPlease, run this program as root!\n\e[0m"
+   printf "\e[1;77mPlease, run this program as root!\n\e[0m" 
    exit 1
 fi
 
